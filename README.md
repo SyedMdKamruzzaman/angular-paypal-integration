349422305ffb258dcba08a20408123931f86df36
Syed Md. Kamruzzaman <syed.kamruzzaman@btracsl.com>
2024-06-07 18:51:22 +0600

    paypal integrated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Paypal
+# AngPaypal
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.3.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,84 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ICreateOrderRequest } from 'ngx-paypal';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'paypal';
+export class AppComponent implements OnInit {
+  title = 'ang-paypal';
+  public payPalConfig: any;
+
+  ngOnInit() {
+    this.payPalConfig = {
+      currency: "EUR",
+      clientId: "Ac9-UbBL0TcehRW-l4qSP1MCHvYkpMDaPSWNEWF9wk0OzsRAH-lGRO768k7TwRWgV0txWPyNstdA4fY-",
+      createOrder: (data:any) =>
+        <ICreateOrderRequest>{
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              amount: {
+                currency_code: "EUR",
+                value: "9.99",
+                breakdown: {
+                  item_total: {
+                    currency_code: "EUR",
+                    value: "9.99"
+                  }
+                }
+              },
+              items: [
+                {
+                  name: "Enterprise Subscription",
+                  quantity: "1",
+                  category: "DIGITAL_GOODS",
+                  unit_amount: {
+                    currency_code: "EUR",
+                    value: "9.99"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+      advanced: {
+        commit: "true"
+      },
+      style: {
+        label: "paypal",
+        layout: "vertical"
+      },
+      onApprove: (data:any, actions:any) => {
+        console.log(
+          "onApprove - transaction was approved, but not authorized",
+          data,
+          actions
+        );
+        actions.order.get().then((details:any) => {
+          console.log(
+            "onApprove - you can get full order details inside onApprove: ",
+            details
+          );
+        });
+      },
+      onClientAuthorization: (data:any) => {
+        console.log(
+          "onClientAuthorization - you should probably inform your server about completed transaction at this point",
+          data
+        );
+      },
+      onCancel: (data:any, actions:any) => {
+        console.log("OnCancel", data, actions);
+      },
+      onError: (err:any) => {
+        console.log("OnError", err);
+      },
+      onClick: (data:any, actions:any) => {
+        console.log("onClick", data, actions);
+      }
+    };
+  }
+ 
 }
